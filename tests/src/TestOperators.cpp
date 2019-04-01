cb33c710631c601945a491f4f622c2e419ab1b48
George Fotopoulos <xorz57@protonmail.com>
2019-04-01 16:35:29 +0300

    Rename Operators Test Source File

---
+++ tests/src/TestOperators.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <EasyCL/EasyCL.hpp>
+
+TEST_CASE("Testing variable operators"){
+    SECTION("Unary"){
+        ecl::Variable<int> a(1);
+        REQUIRE(a++ == 2);
+        REQUIRE(a-- == 1);
+    }
+
+    SECTION("Binary"){
+        ecl::Variable<int> a(1);
+        REQUIRE((a = 3) == 3);
+        REQUIRE((a += 4) == 7);
+        REQUIRE((a -= 3) == 4);
+        REQUIRE((a *= 2) == 8);
+        REQUIRE((a /= 4) == 2);
+    }
+}
+
+TEST_CASE("Testing array operators"){
+    ecl::Array<int> a(new int[5]{1, 2, 3, 4, 5}, 5, ecl::CONTROL::FREE);
+    for(size_t i = 0; i < 5; i++)
+        REQUIRE(a[i] == i + 1);
+}
