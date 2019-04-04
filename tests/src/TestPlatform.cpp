f06fb1711df432c9b6d2e17358a4b0bf7879a60e
George Fotopoulos <xorz57@protonmail.com>
2019-04-04 18:41:20 +0300

    Create TestPlatform.cpp

---
+++ tests/src/TestPlatform.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <EasyCL/EasyCL.hpp>
+
+TEST_CASE("Default Constructor") {
+	SECTION("Get Device") {
+		// TODO
+	}
+	SECTION("Get Platform Information") {
+		// TODO
+	}
+	SECTION("Get Device Information") {
+		// TODO
+	}
+}
+
+TEST_CASE("Overloaded Constructor 1") {
+	// TODO
+}
