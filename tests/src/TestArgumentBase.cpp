eeb81df87ad7c3b03cf122e8ac68f0195f6c4698
George Fotopoulos <xorz57@protonmail.com>
2019-04-04 18:41:37 +0300

    Create TestArgumentBase.cpp

---
+++ tests/src/TestArgumentBase.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <EasyCL/EasyCL.hpp>
+
+TEST_CASE("Default Constructor") {
+	// TODO
+}
+
+TEST_CASE("Overloaded Constructor 1") {
+	// TODO
+}
+
+TEST_CASE("Overloaded Constructor 2") {
+	// TODO
+}
+
+TEST_CASE("Move Constructor") {
+	// TODO
+}
+
+// TODO
